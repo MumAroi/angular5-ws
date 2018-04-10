2b4c78e550a683d0719fd669a64254430a89abb1
nilecon <nilecon@nilecons-Mac-mini.local>
2018-04-10 18:22:46 +0700

    add code

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface User {
+    user_id:      number;
+    first_name:   string;
+    last_name:    string;
+    password:     string;
+    type:         number;
+    is_delete:    number;
+    is_active:    number;
+    phone_number: string;
+    email:        string;
+    created_date: string;
+    updated_date: string;
+}
